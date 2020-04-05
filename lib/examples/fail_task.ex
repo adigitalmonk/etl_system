2ab8583f514b3a3c303ae132b8be4707c3e84b4a
Brad Boudreau <brad.app@gmail.com>
2020-04-05 18:14:44 -0400

    Fixing fail_task example to not reference it's old set up

--- lib/examples/fail_task.ex
+++ lib/examples/fail_task.ex
@@ -1,18 +1,18 @@
 defmodule Examples.FailTask do
   @moduledoc """
-  Example task to show off failing workflows, masquerading as a file loader
+  Example task to show off failing workflows
   """
   use ETLSystem.Task
 
   @doc """
   If the arg given to this task is the string "fail" then ... fail.
-  Otherwise, return a placeholder value and continue
+  Otherwise, return the value of the previous step.
   """
   def run(%{args: "fail"} = workflow) do
     {:err, :given, workflow}
   end
 
   def run(workflow) do
-    {:ok, "delta", workflow}
+    {:ok, workflow.previous, workflow}
   end
 end
